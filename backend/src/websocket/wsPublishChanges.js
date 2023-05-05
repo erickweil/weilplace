aa1c2c80d4b5e98e7f1824ca36d71e29b41a6f94
Erick <erick.weil@ifro.edu.br>
2023-05-05 14:52:46 -0400

    Corrigido problema ao iniciar o websocket publisher

--- backend/src/websocket/wsPublishChanges.js
+++ backend/src/websocket/wsPublishChanges.js
@@ -11,55 +11,64 @@ const doChangesGet = async (last_i) => {
 	else throw new Error("Não foi possível completar a requisição:"+res);
 };
 
-let last_i = 0;
+// TEM QUE COMEÇAR COM -1 PARA PEGAR O ÍNDICE DO ÚLTIMO SAVE E NÃO ATRAVESSAR O HISTÓRICO INTEIRO
+let last_i = -1;
 let last_identifier = false;
 let broadcastCounter = 0;
 // A ideia é ficar verificando se houve modificações, e caso seja detectado modificações faz um broadcasting
 // a todos os usuários que essa modificação aconteceu
 export const setupPublishChanges = (wss) => {
 	const interval = setInterval(async () => {
-
-		if(wss.clients.size <= 0) {
-			// se ninguém vai ouvir para que ficar verificando?
-			return;
-		}
-
-		//console.log("interval last_i:",last_i);
-		const resp = await doChangesGet(last_i);
-
-		if(!resp 
-			|| resp.i === undefined 
-			|| resp.identifier === undefined 
-		) {
-			console.log("Publish Changes: Erro ao realizar changesGet, resposta sem valores necessários:",resp);
-			return false;
-		}
-
-		const i = parseInt(resp.i);
-		const identifier = resp.identifier;
-
-		// Tem que ver isso depois
-		if(last_identifier !== false && identifier != last_identifier) {
-			console.log("Publish Changes: Atualizou identifier. resetando index");
-			last_i = 0;
-			last_identifier = identifier;
-			return false;
-		}
-
-		if(last_i != i || last_identifier != identifier) { 
-			last_i = i;
-			last_identifier = identifier;
-
-			broadcastCounter++;
-			if(broadcastCounter % 1000 == 0) { // faz o log a cada 1000 broadcasts para ter uma ideia da carga
-				const timestamp = new Date().toISOString();
-				console.log(timestamp+" total de broadcasts:"+broadcastCounter+", websockets conectados:"+wss.clients.size);
+		try {
+			if(wss.clients.size <= 0) {
+				// se ninguém vai ouvir para que ficar verificando?
+				return;
 			}
 
-			doBroadcastEveryone(wss,JSON.stringify({
-				post: "/publishchanges",
-				...resp
-			}));
+			//console.log("interval last_i:",last_i);
+			const resp = await doChangesGet(last_i);
+
+			if(!resp 
+				|| resp.i === undefined 
+				|| resp.identifier === undefined 
+			) {
+				console.log("Publish Changes: changesGet deu resposta sem valores necessários:",resp);
+				return;
+			}
+
+			const i = parseInt(resp.i);
+			const identifier = resp.identifier;
+
+			// Tem que ver isso depois
+			if(last_identifier !== false && identifier != last_identifier) {
+				console.log("Publish Changes: Atualizou identifier. resetando index");
+				last_i = -1; // para pegar o index do último save
+				last_identifier = identifier;
+				return;
+			}
+
+			if(last_i != i || last_identifier != identifier) { 
+				last_i = i;
+				last_identifier = identifier;
+
+				if(resp.changes) { // tem mudanças? então faz broadcast
+					broadcastCounter++;
+					if(broadcastCounter % 1000 == 0) { // faz o log a cada 1000 broadcasts para ter uma ideia da carga
+						const timestamp = new Date().toISOString();
+						console.log(timestamp+" total de broadcasts:"+broadcastCounter+", websockets conectados:"+wss.clients.size);
+					}
+
+					doBroadcastEveryone(wss,JSON.stringify({
+						post: "/publishchanges",
+						...resp
+					}));
+				}
+			}
+		} catch(error) {
+			console.error("Publish Changes: Erro! ",error);
+			// Resetando...
+			last_i = -1;
+			last_identifier = false;
 		}
 	}, 50);
 
